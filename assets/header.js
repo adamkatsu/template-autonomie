fb134c35f852f2a05f502aa6edc0c992928cff78
adamkatsu <adamkatsu369@gmail.com>
2024-11-21 08:55:48 +0700

    feat: nav overlay

--- assets/header.js
+++ assets/header.js
@@ -7,15 +7,21 @@ const searchMenu = document.querySelector('.dropdown-right');
 const cartTrigger = document.getElementById('nav-cart');
 const userTrigger = document.getElementById('nav-user');
 
+const navOverlay = document.querySelector('.navbar-overlay');
+
 shopTrigger.addEventListener('mouseenter', () => {
   shopMenu.classList.add('dropdown-active');
+  navOverlay.classList.add('show');
 })
 shopMenu.addEventListener('mouseleave', () => {
   shopMenu.classList.remove('dropdown-active');
+  navOverlay.classList.remove('show');
 })
 searchTrigger.addEventListener('mouseenter', () => {
   searchMenu.classList.add('dropdown-active');
+  navOverlay.classList.add('show');
 })
 searchMenu.addEventListener('mouseleave', () => {
   searchMenu.classList.remove('dropdown-active');
+  navOverlay.classList.remove('show');
 })
