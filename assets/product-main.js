70a7113e5203f4d369eff5c104d13eb6470bb148
adamkatsu <adamkatsu369@gmail.com>
2024-11-23 10:07:59 +0700

    feat: product page interaction and style

--- assets/product-main.js
+++ assets/product-main.js
@@ -12,3 +12,30 @@ decreaseBtn.addEventListener("click", () => {
 increaseBtn.addEventListener("click", () => {
   quantityInput.value = parseInt(quantityInput.value) + 1;
 });
+
+// Accordion
+
+document.querySelectorAll('.product-accordion-head').forEach(header => {
+  header.addEventListener('click', () => {
+    const item = header.parentElement;
+    const content = item.querySelector('.product-accordion-content');
+    const isOpen = item.classList.contains('active');
+
+    // Close all open accordions
+    // document.querySelectorAll('.product-accordion-item').forEach(i => {
+    //   if (i !== item) {
+    //     i.classList.remove('active');
+    //     i.querySelector('.product-accordion-content').style.maxHeight = '0';
+    //   }
+    // });
+
+    // Toggle the clicked accordion
+    if (!isOpen) {
+      item.classList.add('active');
+      content.style.maxHeight = `${content.scrollHeight}px`; // Set to full content height
+    } else {
+      item.classList.remove('active');
+      content.style.maxHeight = '0'; // Collapse content
+    }
+  });
+});
