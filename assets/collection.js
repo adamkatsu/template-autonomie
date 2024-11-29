cc0435cde59e129c57d419fc8f2a762f01761975
adamkatsu <adamkatsu369@gmail.com>
2024-11-29 16:47:18 +0700

    feat: collection filter popup

--- assets/collection.js
+++ assets/collection.js
@@ -30,4 +30,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
   });
+
+  // Show / Hide Filter Modal
+  const filterShow = document.getElementById('filter-show');
+  const filterPopup = document.querySelector('.collection-popup');
+  const filterHide = document.getElementById('filter-hide');
+
+  filterShow.addEventListener('click', () => {
+    filterPopup.classList.add('popup-active');
+  })
+  filterHide.addEventListener('click', () => {
+    filterPopup.classList.remove('popup-active');
+  })
 });
