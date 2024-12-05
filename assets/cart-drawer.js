172393bdc819bce1a0f93a823ced9b04e745ffd0
adamkatsu <adamkatsu369@gmail.com>
2024-12-05 07:17:58 +0700

    feat: collection page variant to trigger cart drawer

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -1,9 +1,11 @@
 function openCartDrawer() {
   document.querySelector('.cart-drawer').classList.add('open');
 }
+
 function closeCartDrawer() {
   document.querySelector('.cart-drawer').classList.remove('open');
 }
+
 async function updateCartDrawer() {
   const res = await fetch('/?section_id=cart-drawer')
   const text = await res.text()
@@ -13,14 +15,90 @@ async function updateCartDrawer() {
 
   const newBox = html.querySelector('.cart-drawer').innerHTML;
   document.querySelector('.cart-drawer').innerHTML = newBox;
-  
+
   console.log(html)
+
+  addCartDrawerListeners();
 }
 
+function addCartDrawerListeners() {
+  // Update quantity with button click
+  document.querySelectorAll('.drawer-cart-product-quantity button').forEach((button) => {
+
+    button.addEventListener('click', async () => {
+      // Get line item key
+      const rootItem = button.parentElement.parentElement;
+      const key = rootItem.getAttribute('data-line-item-key');
+
+      // Get new quantity
+      const currentQuantity = Number(button.parentElement.querySelector('input').value);
+      const isUp = button.classList.contains('increase-quantity');
+
+      const newQuantity = isUp? currentQuantity + 1 : currentQuantity - 1;
+
+      // Ajax update
+      const res = await fetch('/cart/update.js', {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          updates: {[key]: newQuantity}
+        }),
+      });
+
+      // Update cart
+      updateCartDrawer();
+    })
+
+    
+  })
+
+  // Update quantity with manual input 
+  document.querySelectorAll('.drawer-cart-product-quantity input').forEach((input) => {
+    input.addEventListener('input', async () => {
+
+      // Get line item key
+      const rootItem = input.parentElement.parentElement;
+      console.log(rootItem);
+      const key = rootItem.getAttribute('data-line-item-key');
+
+      // Get new quantity
+      const newQuantity = input.value;
+
+      // Ajax update
+      const res = await fetch('/cart/update.js', {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          updates: {[key]: newQuantity}
+        }),
+      });
+
+      // Update cart
+      updateCartDrawer();
+
+    })
+
+  })
+
+  document.querySelectorAll('.close-drawer, .cart-drawer').forEach((x) => {
+    x.addEventListener('click', () => {
+      closeCartDrawer();
+    })
+  });
+  document.querySelector('.drawer').addEventListener('click', (e) => {
+    e.stopPropagation();
+  })
+}
+
+
+// Add to cart button
 document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    console.log('product added!');
 
     // Submit form with ajax
     await fetch("/cart/add.js", {
@@ -36,17 +114,50 @@ document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
   })
 })
 
-document.querySelectorAll('.close-drawer, .cart-drawer').forEach((x) => {
-  x.addEventListener('click', () => {
-    closeCartDrawer();
-  })
+
+// Select all variant buttons
+document.querySelectorAll(".variant-button").forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const variantId = this.getAttribute("data-variant-id");
+    e.preventDefault();
+
+    // Add variant to cart using Shopify AJAX API
+    fetch("/cart/add.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: variantId,
+        quantity: 1,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Redirect to the cart page
+          // Update cart
+          updateCartDrawer();
+
+          // Open cart Drawer
+          openCartDrawer();
+        } else {
+          alert("Error adding to cart. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 });
-document.querySelector('.drawer').addEventListener('click', (e) => {
-  e.stopPropagation();
-})
+
+
+
+// Open cart drawer from nav icon
 document.querySelectorAll('a[href="/cart"]').forEach((a) => {
   a.addEventListener('click', (e) => {
     e.preventDefault();
     openCartDrawer();
   })
 })
+
+addCartDrawerListeners(); 
--- assets/collection.js
+++ assets/collection.js
@@ -1,39 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Select all variant buttons
-  const variantButtons = document.querySelectorAll(".variant-button");
-
-  variantButtons.forEach((button) => {
-    button.addEventListener("click", function (e) {
-      const variantId = this.getAttribute("data-variant-id");
-      e.preventDefault();
-
-      // Add variant to cart using Shopify AJAX API
-      fetch("/cart/add.js", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: variantId,
-          quantity: 1, // Adjust quantity as needed
-        }),
-      })
-        .then((response) => {
-          if (response.ok) {
-            // Redirect to the cart page
-            // window.location.href = '/cart'
-            console.log('product added!');
-          } else {
-            alert("Error adding to cart. Please try again.");
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    });
-  });
-
-
   // Show / Hide Filter Modal
   const filterShow = document.getElementById('filter-show');
   const filterPopup = document.querySelector('.collection-popup');
