05afe8e1ff2a6c5009c69f5080bfbd7c23c5edc4
adamkatsu <adamkatsu369@gmail.com>
2025-01-13 13:19:12 +0700

    feat: collection sort

--- assets/collection.js
+++ assets/collection.js
@@ -52,3 +52,71 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdown = document.querySelector('.dropdown');
+  const trigger = dropdown.querySelector('.dropdown-trigger');
+  const options = dropdown.querySelectorAll('.dropdown-option');
+  const hiddenInput = dropdown.querySelector('#sort-by');
+  const form = dropdown.closest('form');
+
+  const dropdownMobile = document.querySelector('.sort-mobile');
+  const optionsMobile = dropdownMobile.querySelectorAll('.dropdown-mobile-option');
+  const hiddenInputMobile = dropdownMobile.querySelector('#sort-by-mobile');
+  const formMobile = dropdownMobile.closest('form');
+
+
+  if (window.matchMedia("(min-width: 481px)").matches) {
+    console.log('desktop')
+    
+
+    trigger.addEventListener('click', () => {
+      console.log('click ok')
+      dropdown.classList.toggle('open');
+    });
+
+    options.forEach(option => {
+      option.addEventListener('click', () => {
+        const value = option.dataset.value;
+        hiddenInput.value = value;
+        form.submit();
+      });
+    });
+
+    // Close dropdown when clicking outside
+    document.addEventListener('click', (event) => {
+      if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove('open');
+      }
+    });
+  }
+  
+  if(window.matchMedia("(max-width: 480px)").matches) {
+    console.log('mobile')
+    
+    trigger.addEventListener('click', () => {
+      dropdownMobile.classList.toggle('open');
+    });
+
+    optionsMobile.forEach(option => {
+      console.log(option)
+      option.addEventListener('click', () => {
+        console.log('click ok')
+        const value = option.dataset.value;
+        hiddenInput.value = value;
+        form.submit();
+      });
+    });
+
+    // Close dropdown when clicking outside
+    dropdownMobile.addEventListener('click', () => {
+      dropdownMobile.classList.remove('open');
+    })
+    document.querySelector('.sort-mobile-hide-btn').addEventListener('click', () => {
+      dropdownMobile.classList.remove('open');
+    })
+    document.querySelector('.form-mobile').addEventListener('click', (e) => {
+      e.stopPropagation();
+    })
+  }
+});
